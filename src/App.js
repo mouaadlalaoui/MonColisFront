89492d269d50e7eb4800bf6c39b4022dca799560
Amran_zak <80887236+amran-zak@users.noreply.github.com>
2022-11-17 11:29:34 +0000

    test router

--- src/App.js
+++ src/App.js
@@ -1,16 +1,28 @@
 import logo from './logo.svg';
 import './App.css';
 import Inscription from './Components/Inscription';
+
+import { Routes, Route } from "react-router-dom";
 /*import './Services/inscription';
 */
-
+const Test = () => {
+  return (
+    <div>
+      <h1>Coucou</h1>
+    </div>
+  )
+}
 
 function App() {
   return (
     <div className="App">
-      
-        <Inscription/>
 
+        <Inscription/>
+        <Test/>
+{/* <Link to="/test"//// */}
+<Routes>
+            <Route  path="/test" element={<Test/>} />
+            </Routes>
     </div>
   );
 }
