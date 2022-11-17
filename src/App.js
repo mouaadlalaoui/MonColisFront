98cdce5c641b67c1e627e7da4b499263bea0c3e5
mouaadlalaoui <82550350+mouaadlalaoui@users.noreply.github.com>
2022-11-17 11:53:22 +0100

    Revert "voici la mise à jour récente"

--- src/App.js
+++ src/App.js
@@ -1,16 +1,25 @@
 import logo from './logo.svg';
 import './App.css';
-import Inscription from './Components/Inscription';
-/*import './Services/inscription';
-*/
-
 
 function App() {
   return (
     <div className="App">
-      
-        <Inscription/>
-
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <h1>gggg</h1>
+        <h2> Bonjour  Amrane cava?????</h2>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
     </div>
   );
 }
